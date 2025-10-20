09d5be50dc2b2bbfaa38ce250027e06f43ace381
makpol83 <maksimiliano.pok178@gmail.com>
2025-10-20 12:30:20 +0200

    Added db_modify_register_with_PK, now a register column can be changed with the PK, table_name and column_name, tested

--- processing_app/modify_register.py
+++ processing_app/modify_register.py
@@ -1,40 +1,23 @@
 import psycopg
 import connection
 import data_insertion_removal
+import get_registers
 from enums_dicts import PRIMARY_KEYS
 
-# Returns all the registers from the table name received
-# Returns a list of dictionaries where each list is a dictionary with column_name | value
-def get_registers_from_table(conn, table_name: str):
-
+def db_modify_register_with_PK(conn, table_name, column_name, new_value, primary_keys: list):
     # We configure row_factory to access name per column
     conn.row_factory = psycopg.rows.dict_row
     cur = conn.cursor()
     
     try:
-        query = f'SELECT * FROM "{table_name}";'
-        cur.execute(query)
-        registros = cur.fetchall()  # Lista de diccionarios
-        return registros
-    except Exception as e:
-        print(f"Error al obtener registros de {table_name}: {e}")
-        return []
-    finally:
-        cur.close()
+        cur.execute("BEGIN;")
+        query = "UPDATE " + table_name + " SET "  + column_name + " = " + new_value + " WHERE "
 
-# Returns the register with the PK received by argument
-def get_registers_from_table_with_PK(conn, table_name: str, primary_keys: list):
-
-    # We configure row_factory to access name per column
-    conn.row_factory = psycopg.rows.dict_row
-    cur = conn.cursor()
-    
-    try:
-        query = "SELECT * FROM "  + table_name +" WHERE "
         lst = PRIMARY_KEYS[table_name]
         length = len(primary_keys)
         if len(lst) != length:
             raise Exception
+
         if length == 1:
             query += lst[0] + " = " + primary_keys[0] + ";"
         else:
@@ -48,10 +31,10 @@ def get_registers_from_table_with_PK(conn, table_name: str, primary_keys: list):
                 query += primary_keys[i] + ", "
             query += primary_keys[length - 1] + ");"
         cur.execute(query)
-        register = cur.fetchall()  # List of diccionaries
-        return register
+        cur.execute("COMMIT;")
+        
     except Exception as e:
-        print(f"Error when getting registers from {table_name}: {e}")
+        print(f"Error when modifying register from {table_name}: {e}")
         return []
     finally:
         cur.close()
@@ -60,9 +43,12 @@ def get_registers_from_table_with_PK(conn, table_name: str, primary_keys: list):
 conn = connection.create_connection()
 #data_insertion_removal.db_insert_register_on_table(conn, ['don', 'name', 'EE_use', 'description'], ['DON_TEST', '15', 'DESCR_TEST'])
 #data_insertion_removal.db_insert_register_on_table(conn, ['don', 'name', 'EE_use', 'description'], ['DON_TEST', '16', 'DESCR_TEST'])
-#lst_all = get_registers_from_table(conn, 'don')
+#lst_all = get_registers.get_registers_from_table(conn, 'don')
 #print(f'{lst_all}')
-#lst = get_registers_from_table_with_PK(conn, 'don', ['13'])
+#lst = get_registers.get_registers_from_table_with_PK(conn, 'don', ['14'])
+#print(f'{lst}')
+#db_modify_register_with_PK(conn, 'don', 'name', '\'TEST_CHANGES?\'', ['14'])
+#lst = get_registers.get_registers_from_table_with_PK(conn, 'don', ['14'])
 #print(f'{lst}')
 #data_insertion_removal.db_clear_register_on_table_on_cascade(conn, 'don', {"id_d": 5})
 #data_insertion_removal.db_clear_table_on_cascade(conn, 'don')
